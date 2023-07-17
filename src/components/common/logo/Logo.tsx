c506892ffa13878d1f1e0248680cda772f4fcd2d
Kimbangul <highcolor_12@naver.com>
2023-07-17 09:37:57 +0900

    style: add header components

---
+++ src/components/common/logo/Logo.tsx
@@ -0,0 +1,13 @@
+import LOGOIMG from 'assets/image/common/craft-full-black.svg';
+
+const Logo = () => {
+  return (
+    <div>
+      <a href='#'>
+        <LOGOIMG />
+      </a>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import Logo from 'components/common/logo/Logo';
+// import NavIcon from 'assets/image/common/nav-icon.svg';
+
+const Header = () => {
+  return (
+    <header>
+      <Logo />
+      <button>{/* <NavIcon /> */}</button>
+      <button>CONTACT</button>
+    </header>
+  );
+};
+
+export default Header;
