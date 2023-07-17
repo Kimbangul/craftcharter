4ef9bc99272ac2e602aead19543eb9e92325de18
Kimbangul <highcolor_12@naver.com>
2023-07-17 17:00:01 +0900

    style: add assets

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,9 +1,22 @@
 import MAIN_BG01 from '../../assets/video/main-bg.mp4';
 import MAIN_BG02 from '../../assets/video/main-bg02.mp4';
+import { ReactComponent as MainIcon } from 'assets/image/common/logo-icon.svg';
+import { ReactComponent as ARROW_DOWN } from 'assets/image/main/arrow-down.svg';
 
 const Main = () => {
   return (
     <section className='Main'>
+      <div className='Main__logo'>
+        <MainIcon />
+      </div>
+      <div className='Main__btn--next'>
+        <p className='Main__text'>
+          <span className='Main__text-line'>fly your way</span>
+          <span className='Main__text-line'>Craft the perfect journey</span>
+        </p>
+        <ARROW_DOWN />
+      </div>
+
       <video className='Main__video' autoPlay={true} muted loop playsInline>
         <source src={MAIN_BG01} />
         <source src={MAIN_BG02} />
