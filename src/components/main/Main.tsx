902ebdc1bdc5742472061fcd8e993192158ec074
Kimbangul <highcolor_12@naver.com>
2023-07-24 16:31:58 +0900

    feat: add main scroll feat

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,15 +1,60 @@
+import { useEffect, useRef } from 'react';
 import MAIN_BG01 from '../../assets/video/main-bg.mp4';
 import MAIN_BG02 from '../../assets/video/main-bg02.mp4';
 import { ReactComponent as MainIcon } from 'assets/image/common/logo-icon.svg';
 import { ReactComponent as ARROW_DOWN } from 'assets/image/main/arrow-down.svg';
 
 const Main = () => {
+  const mainRef = useRef<null | HTMLDivElement>(null);
+
+  const onScrollMain = (e: Event | WheelEvent) => {
+    if (e instanceof WheelEvent && e.deltaY > 0) {
+      scrollToNext();
+    }
+  };
+
+  const onObserveMain = (entry: IntersectionObserverEntry[]) => {
+    // console.log(entry[0].intersectionRatio);
+    if (entry[0].intersectionRatio <= 0) {
+      document.body.style.height = 'auto';
+      document.body.style.overflowY = 'auto';
+      document.removeEventListener('wheel', onScrollMain);
+    } else if (entry[0].intersectionRatio >= 0.8) {
+      document.body.style.height = '100vh';
+      document.body.style.overflowY = 'hidden';
+      document.addEventListener('wheel', onScrollMain);
+    }
+  };
+
+  const mainObserver = new IntersectionObserver(
+    (entry) => {
+      onObserveMain(entry);
+    },
+    {
+      root: null,
+      rootMargin: '0px',
+      threshold: [1.0, 0.8, 0],
+    }
+  );
+
+  useEffect(() => {
+    if (!mainRef.current) return;
+    mainObserver.observe(mainRef.current);
+  }, []);
+
+  const scrollToNext = () => {
+    const introduce = document.querySelector('.Introduce');
+    if (!introduce) return;
+
+    introduce.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  };
+
   return (
-    <section className='Main'>
+    <section className='Main' ref={mainRef}>
       <div className='Main__logo'>
         <MainIcon />
       </div>
-      <div className='Main__btn--next'>
+      <div className='Main__btn--next' onClick={scrollToNext}>
         <p className='Main__text'>
           <span className='Main__text-line'>fly your way</span>
           <span className='Main__text-line'>Craft the perfect journey</span>
