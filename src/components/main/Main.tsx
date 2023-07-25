14f4540a73d9f36a5f509569aaf198d2291b8e60
Kimbangul <highcolor_12@naver.com>
2023-07-26 08:39:26 +0900

    style: add media query

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -10,21 +10,11 @@ const Main = () => {
   const mainRef = useRef<null | HTMLDivElement>(null);
 
   const scroll = useScroll();
+
   const scrollDir = useMemo(() => {
+    console.log(scroll.scrollDir);
     return scroll.scrollDir;
   }, [scroll.scrollDir]);
-
-  const observer = useObserver(
-    (entry: IntersectionObserverEntry[]) => {
-      if (entry[0].isIntersecting) {
-        // document.addEventListener('scroll', onMoveSection);
-      } else {
-        // document.removeEventListener('scroll', onMoveSection);
-      }
-      // console.log(entry[0].isIntersecting);
-    },
-    { threshold: 0.5 }
-  );
 
   const onMoveSection = useCallback(() => {
     console.log(scrollDir);
@@ -38,38 +28,39 @@ const Main = () => {
     }
   }, [scrollDir]);
 
-  // useEffect(() => {
-  //   console.log(scroll.scrollDir);
+  useEffect(() => {
+    onMoveSection();
+  }, [onMoveSection]);
 
-  //   if (scroll.scrollDir === 'up') {
-  //     window.scrollTo({ top: 0 });
-  //   }
-
-  //   if (scroll.scrollDir === 'down') {
-  //     console.log(window.innerHeight);
-  //     window.scrollTo({ top: window.innerHeight });
-  //   }
-  // }, [scroll.scroll, scroll.scrollDir]);
+  const observer = useObserver(
+    (entry: IntersectionObserverEntry[]) => {
+      if (entry[0].isIntersecting) {
+        // document.addEventListener('scroll', onMoveSection);
+      } else {
+        // document.removeEventListener('scroll', onMoveSection);
+      }
+      // console.log(entry[0].isIntersecting);
+    },
+    { threshold: 0.5 }
+  );
 
   return (
     <section className='Main' ref={observer.target}>
-      <div className='Main__inner'>
-        <div className='Main__logo'>
-          <MainIcon />
-        </div>
-        <div className='Main__btn--next'>
-          <p className='Main__text'>
-            <span className='Main__text-line'>fly your way</span>
-            <span className='Main__text-line'>Craft the perfect journey</span>
-          </p>
-          <ARROW_DOWN />
-        </div>
-
-        <video className='Main__video' autoPlay={true} muted loop playsInline>
-          <source src={MAIN_BG01} />
-          <source src={MAIN_BG02} />
-        </video>
+      <div className='Main__logo'>
+        <MainIcon />
       </div>
+      <div className='Main__btn--next'>
+        <p className='Main__text'>
+          <span className='Main__text-line'>fly your way</span>
+          <span className='Main__text-line'>Craft the perfect journey</span>
+        </p>
+        <ARROW_DOWN />
+      </div>
+
+      <video className='Main__video' autoPlay={true} muted loop playsInline>
+        <source src={MAIN_BG01} />
+        <source src={MAIN_BG02} />
+      </video>
     </section>
   );
 };
